0ea2d2228a0c2e0757f40567b95b9d59db20ccb0
JaeGeunBang <jgb710@naver.com>
2019-05-07 15:59:19 +0900

    add Bin2Dec Vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Bin2Dec from '@/components/Bin2Dec'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Bin2Dec',
+      component: Bin2Dec
     }
   ]
 })
